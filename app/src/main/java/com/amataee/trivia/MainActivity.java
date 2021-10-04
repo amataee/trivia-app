f9678dbe936a6956e2a923e893f923495d645a0d
amataee <am.ataee@outlook.com>
2021-10-04 20:38:12 +0330

    first setup

--- app/src/main/java/com/amataee/trivia/MainActivity.java
+++ app/src/main/java/com/amataee/trivia/MainActivity.java
@@ -3,12 +3,28 @@ package com.amataee.trivia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.amataee.trivia.controller.AppController;
+import com.android.volley.Request;
+import com.android.volley.toolbox.JsonArrayRequest;
 
 public class MainActivity extends AppCompatActivity {
+    String url = "https://raw.githubusercontent.com/curiousily/simple-quiz/master/script/statements-data.json";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                response -> {
+                    Log.d("TAG", "onCreate: Yeah!");
+                }, error -> {
+
+        });
+
+        AppController.getInstance().addToRequestQueue(jsonArrayRequest);
+
     }
 }
