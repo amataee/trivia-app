fa282dbd32294748744aaece9cb13048f31a3003
amataee <am.ataee@outlook.com>
2021-10-05 00:58:32 +0330

    Setup Data Binding View and Show Next Question

--- app/src/main/java/com/amataee/trivia/MainActivity.java
+++ app/src/main/java/com/amataee/trivia/MainActivity.java
@@ -1,13 +1,16 @@
 package com.amataee.trivia;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.amataee.trivia.controller.AppController;
 import com.amataee.trivia.data.AnswerListAsyncResponse;
 import com.amataee.trivia.data.Repository;
+import com.amataee.trivia.databinding.ActivityMainBinding;
 import com.amataee.trivia.model.Question;
 import com.android.volley.Request;
 import com.android.volley.toolbox.JsonArrayRequest;
@@ -16,15 +19,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    List<Question> questionList;
+    private int currentQuestionIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        List<Question> questions = new Repository().getQuestions(questionArrayList -> {
-            Log.d("TAG", "onCreate: " + questionArrayList);
+        questionList = new Repository().getQuestions(questionArrayList -> {
+            binding.questionTextView.setText(questionArrayList.get(currentQuestionIndex).getAnswer());
+            });
+
+        binding.buttonNext.setOnClickListener(view -> {
+            currentQuestionIndex = (currentQuestionIndex + 1) %  questionList.size();
+            updateQuestion();
         });
 
+        binding.buttonTrue.setOnClickListener(view -> {
+
+        });
+
+        binding.buttonTrue.setOnClickListener(view -> {
+
+        });
+
+    }
+
+    private void updateQuestion() {
+        String question = questionList.get(currentQuestionIndex).getAnswer();
+        binding.questionTextView.setText(question);
     }
 }
